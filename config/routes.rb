d0ab080d699d5ead2449011b40cb250d79b382de
Thomas Marignan <marignanthomas10@gmail.com>
2021-02-08 14:05:02 +0100

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
